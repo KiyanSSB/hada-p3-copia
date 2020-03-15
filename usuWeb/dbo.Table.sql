da4df494f1d04db7ae6d2acd8f56e0e647641a45
Gabriel <gdld1@alu.ua.es>
2020-03-15 16:43:18 +0100

    45929352P - Agregado Database.mdf y dbo.Table.sql

---
+++ usuWeb/dbo.Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[id] INT IDENTITY (1, 1) NOT NULL, 
+	[nombre] NVARCHAR (32) NOT NULL, 
+	[nif] NVARCHAR (16) NOT NULL, 
+	[edad] INT NOT NULL, 
+	PRIMARY KEY CLUSTERED ([id] ASC), 
+	UNIQUE NONCLUSTERED ([nif] ASC)
+)
