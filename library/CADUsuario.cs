c8ea5232e34cfb9e6aae0ad265f3c35408c5463d
Gabriel <gdld1@alu.ua.es>
2020-03-30 18:27:33 +0200

    45929352P - Añadidas descripciones en EN y CAD

--- library/CADUsuario.cs
+++ library/CADUsuario.cs
@@ -3,10 +3,114 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace library
 {
     public class CADUsuario
-    {
+    {   
+        /// <summary>
+        /// Contiene la cadena de conexión a la base de datos (Configurada en Web.config)
+        /// </summary>
+        private string constring;
+
+        /// <summary>
+        /// Inicializa la cadena de conexión de la BD
+        /// </summary>
+        public CADUsuario()
+        {
+            constring = ConfigurationManager.ConnectionStrings["DatabaseConnection"].ToString();
+        }
+
+        /// <summary>
+        /// Crea un nuevo usuario en la BD con los datos del usuario representado por el parámetro en
+        /// </summary>
+        /// <param name="en">
+        /// Representa el EN del usuario, se modifican los valores por parametro de manera predeterminada
+        /// </param>
+        /// <returns>
+        /// Devuelve false si se ha producido algún eror, true en caso contrario;
+        /// </returns>
+        public bool createUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Devuelve solo el usuairo indicado leído de la DB
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>
+        /// False si se produce un error
+        /// </returns>
+        public bool readUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Devuelve solo el primer usuario de la BD
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>False si se produce un error</returns>
+        public bool readFirstUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Devuelve solo el usuario siguiente al indicado
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>False si se produce un error</returns>
+        public bool readNextUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Devuelve solo el usuario anterior al indicado
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>False si se produce un error</returns>
+        public bool readPrevUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Actualiza los datos de un usuario en la BD con los datos del usuario representado por el parámetro
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>False si se produce un error</returns>
+        public bool updateUsuario(ENUsuario en)
+        {
+
+        }
+
+        /// <summary>
+        /// Borra el usuario representado por "en" de la DB
+        /// </summary>
+        /// <param name="en">
+        /// Representa el usuario introducido
+        /// </param>
+        /// <returns>False si se produce un error</returns>
+        public bool deleteUsuario(ENUsuario en)
+        {
+
+        }
     }
 }
--- library/ENUsuario.cs
+++ library/ENUsuario.cs
@@ -3,10 +3,157 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace library
 {
     class ENUsuario
     {
+        private string nif;
+        private string nombre;
+        private int edad;
+
+        /// <summary>
+        /// Propiedad pública con campos de respaldo para NIF
+        /// </summary>
+        public string nifUser
+        {
+            get
+            {
+                return nif;
+            }
+            set
+            {
+                nif = value;
+            }
+        }
+
+        /// <summary>
+        /// Propiedad pública con campos de respaldo para Nombre
+        /// </summary>
+        public string nombreUser
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        /// <summary>
+        /// Propiedad pública con campos de respaldo para Edad
+        /// </summary>
+        public int edadUser
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = value;
+            }
+        }
+
+
+        public ENUsuario()
+        {
+
+        }
+        public ENUsuario(string nif, string nombre, int edad)
+        {
+            this.nif = nif;
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        /// <summary>
+        /// Guarda este usuario en la BD.
+        /// Para ello hará uso de los métodos apropiados de​ CADUsuario​. 
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación.
+        /// </returns>
+        public bool createUsuario()
+        {
+            CADUsuario cad = new CADUsuario();
+        }
+
+        /// <summary>
+        /// Recupera el usuario indicado de la BD. 
+        /// Para ello hará uso de los métodos apropiados de​CADUsuario​.
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación.True si se ha podido.
+        /// </returns>
+        public bool readUsuario()
+        {
+
+        }
+
+        /// <summary>
+        ///  ​Recupera todos los usuarios de la BD y devuelve solo el primer usuario.
+        ///  Para ello hará uso de los métodos apropiados de ​CADUsuario​.
+        /// </summary>
+        /// <returns>
+        ///  Devuelve false si no se ha podido realizar la operación.
+        /// </returns>
+        public bool readFirstUsuario()
+        {
+
+        }
+
+        /// <summary>
+        /// ​Recupera todos los usuarios de la BD y devuelve solo el usuario siguiente al indicado. 
+        /// Para ello hará uso de los métodos apropiados de ​CADUsuario​.
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación. 
+        /// </returns>
+        public bool readNextUsuario()
+        {
+
+        }
+
+        /// <summary>
+        /// ​Recupera todos los usuarios de la BD y devuelve solo el usuario anterior al indicado. 
+        /// Para ello hará uso de los métodos apropiados de ​CADUsuario​. 
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación.
+        /// </returns>
+        public bool readPrevUsuario()
+        {
+
+        }
+
+        /// <summary>
+        /// ​Actualiza este usuario en la BD.
+        /// Para ello hará uso de los métodos apropiados de ​CADUsuario​.         
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación. 
+        /// </returns>
+        public bool updateUsuario()
+        {
+
+        }
+
+        /// <summary>
+        /// ​Borra este usuario de la BD.
+        /// Para ello hará uso de los métodos apropiados de ​CADUsuario​. ///
+        /// </summary>
+        /// <returns>
+        /// Devuelve false si no se ha podido realizar la operación. 
+        /// </returns>
+        public bool deleteUsuario()
+        {
+
+        }
     }
 }
